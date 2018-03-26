8ee80af1d04acc5e19d223d3de38de017d4dc644
Jesus Guerrero <jesusant.guerrero@gmail.com>
2018-03-26 01:36:32 -0400

    finish Task CRUD frontend

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -8,7 +8,7 @@
 			$this->view->disable();
 		}
 
-		public function create() {
+		public function createAction() {
 			{
 				$task = new Tasks();
 				$success = $task->save(
@@ -20,7 +20,7 @@
 				);
 
 				if ($success) {
-					echo json_encode[(['success' => 'saved'])];
+					echo json_encode(['success' => 'saved']);
 				} else {
 					echo json_encode(['error' => json_encode($task->getMessages())]);
 				}
@@ -28,9 +28,25 @@
 			}
 		}
 
-		public function delete($id) {
+		public function updateAction($id) {
 			if ($id) {
-				$task = Tasks::find("id='$id'");
+				$task = Tasks::findFirst("id='$id'");
+				$task->name = $this->request->getPost()['name'];
+				$task->state = $this->request->getPost()['state'];
+				$success = $task->update();
+
+				if ($success) {
+					echo json_encode(['success' => 'saved']);
+				} else {
+					echo json_encode(['error' => json_encode($task->getMessages())]);
+				}
+				$this->view->disable();
+			}
+		}
+
+		public function deleteAction($id) {
+			if ($id) {
+				$task = Tasks::findFirst("id='$id'");
 				$success = $task->delete();
 				echo $success;
 			}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg=" Phalcon-vue Tasker"/>
+
+		<p>
+			<input placeholder="write a new task" v-model="task.name">
+			<button @click="addTask">Save</button>
+    </p>
+
+		<ul>
+			<li :key="task.id" v-for="task of tasks">
+				<span v-if="!task.editMode">{{ task.name }} | {{ task.state }} </span>
+				<input v-else v-model="task.name"/>
+				<button @click="changeMode(task)"> {{ task.editMode ? 'Save' : 'Edit'}}</button>
+				<button @click="deleteTask(task.id)">x</button>
+			</li>
+		</ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from 'axios';
+
+const Task = {
+	name: '',
+	state: 'todo'
+}
 
 export default {
   name: "home",
   components: {
     HelloWorld
-  }
+	},
+	data() {
+		return {
+			tasks: [],
+			task: {...Task}
+		}
+	},
+
+	mounted() {
+		this.getTasks();
+	},
+
+	methods: {
+		async getTasks() {
+			const { data } = await axios.get('/task')
+			data.map((task) => {
+				task.editMode = false;
+				return task;
+			})
+			this.tasks = data;
+		},
+
+		addTask() {
+			const data = {...this.task}
+			const form = new FormData();
+			form.set('name', data.name)
+			form.set('state', data.state)
+			axios.post('/task/create', form)
+				.then(({ data }) => {
+					if (data.success) {
+						this.getTasks();
+						this.task = { ...Task}
+					}
+				})
+				.catch(err => console.log(err))
+		},
+
+		deleteTask(id) {
+			axios.delete(`/task/delete/${id}`)
+				.then(() => this.getTasks());
+		},
+
+		changeMode(task) {
+			if (task.editMode) {
+				this.update(task)
+			}
+			task.editMode = !task.editMode
+		},
+
+		update(task) {
+			const form = new FormData();
+			form.set('id', task.id)
+			form.set('name', task.name)
+			form.set('state', task.state)
+			axios.post(`/task/update/${task.id}`, form)
+				.then(({ data }) => {
+					if (data.success) {
+						alert('updated');
+					}
+				})
+				.catch(err => console.log(err))
+			this.axios
+		}
+	},
+
+
 };
 </script>
